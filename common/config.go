200b5227e3dc06fe24c2317be19f845bda8e7c64
Derek <derek@polymerlabs.org>
2024-08-30 12:30:28 -0700

    add ability to set ttls by method name

--- common/config.go
+++ common/config.go
@@ -47,6 +47,7 @@ type ConnectorConfig struct {
 	Redis      *RedisConnectorConfig      `yaml:"redis" json:"redis"`
 	DynamoDB   *DynamoDBConnectorConfig   `yaml:"dynamodb" json:"dynamodb"`
 	PostgreSQL *PostgreSQLConnectorConfig `yaml:"postgresql" json:"postgresql"`
+	Methods    []*MethodCacheConfig       `yaml:"methods" json:"methods"`
 }
 
 type MemoryConnectorConfig struct {
@@ -74,6 +75,11 @@ func (r *RedisConnectorConfig) MarshalJSON() ([]byte, error) {
 		"password": "REDACTED",
 		"db":       r.DB,
 	})
+}
+
+type MethodCacheConfig struct {
+	Method string `yaml:"method" json:"method"`
+	TTL    string `yamle:"ttl" json:"ttl"`
 }
 
 type DynamoDBConnectorConfig struct {
--- erpc/evm_json_rpc_cache.go
+++ erpc/evm_json_rpc_cache.go
@@ -35,6 +35,13 @@ func NewEvmJsonRpcCache(ctx context.Context, logger *zerolog.Logger, cfg *common
 		return nil, err
 	}
 
+	// set TTL method overrides
+	for _, cacheInfo := range cfg.Methods {
+		if err := c.SetTTL(cacheInfo.Method, cacheInfo.TTL); err != nil {
+			return nil, err
+		}
+	}
+
 	return &EvmJsonRpcCache{
 		conn:   c,
 		logger: logger,
@@ -56,13 +63,15 @@ func (c *EvmJsonRpcCache) Get(ctx context.Context, req *common.NormalizedRequest
 		return nil, err
 	}
 
+	hasTTL := c.conn.HasTTL(rpcReq.Method)
+
 	req.Mu.RLock()
 	blockRef, blockNumber, err := common.ExtractEvmBlockReferenceFromRequest(rpcReq)
 	req.Mu.RUnlock()
 	if err != nil {
 		return nil, err
 	}
-	if blockRef == "" && blockNumber == 0 {
+	if blockRef == "" && blockNumber == 0 && !hasTTL {
 		return nil, nil
 	}
 	if blockNumber != 0 {
@@ -129,7 +138,9 @@ func (c *EvmJsonRpcCache) Set(ctx context.Context, req *common.NormalizedRequest
 		return err
 	}
 
-	if blockRef == "" && blockNumber == 0 {
+	hasTTL := c.conn.HasTTL(rpcReq.Method)
+
+	if blockRef == "" && blockNumber == 0 && !hasTTL {
 		// Do not cache if we can't resolve a block reference (e.g. latest block requests)
 		lg.Debug().
 			Str("blockRef", blockRef).
